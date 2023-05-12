888585a1fce36749cb8d19c089debbc2ee1ea2d9
tokaretin <tokaretin@gmail.com>
2023-05-12 08:46:46 +0200

    Добавьте файлы проекта.

---
+++ SimpleCode_while/SimpleCode_while.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 0;
+
+    while (a < 10) { // пока это действие - выражение правда true
+        cout << "Massage " << a << '\n';
+        a++; // икремент
+    }
+}
